8b38c787c8739820751c37f7483ac037a78f4e93
anak10thn <linux@toroo.org>
2012-04-03 12:44:35 +0700

    push source

---
+++ android/src/app.java
@@ -0,0 +1,16 @@
+package org.tcake.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+@SuppressWarnings("unused")
+public class app extends DroidGap 
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
